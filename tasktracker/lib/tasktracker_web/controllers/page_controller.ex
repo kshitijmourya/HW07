3a074b9c6f53c33816e2083975a1e1526e0ef75b
kshitijmourya <mourya.k@husky.neu.edu>
2018-02-27 04:19:42 -0500

    Initial

---
+++ tasktracker/lib/tasktracker_web/controllers/page_controller.ex
@@ -0,0 +1,14 @@
+defmodule TasktrackerWeb.PageController do
+  use TasktrackerWeb, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+#Attribution: Similar to Microblog class example
+  def usertasks(conn, _params) do
+    tasks = Tasktracker.Social.list_tasks()
+    changeset = Tasktracker.Social.change_task(%Tasktracker.Social.Task{})
+    render conn, "usertasks.html", tasks: tasks, changeset: changeset
+  end
+end
